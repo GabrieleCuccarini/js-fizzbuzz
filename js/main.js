9e64a589797b2647d023a85bf3cbb680379dfdfa
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-02 17:38:18 +0100

    Fixed code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
+const nDiQuadrati = 100;
+const boardElementBox = document.querySelector("scacchiera");
 
 // Ipposto il ciclo for per conteggiare da 1 a 100
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= nDiQuadrati; i++) {
+
+    // boardElementBox.innerHTML += `<div class="quadrato">${i}</div>`;
 
     // Verifico i multipli di 3
-    if (i % 3 == 0)
+    if (i % 3 === 0 && i % 15 !== 0) {
         console.log("Fizz")
+    }
     // Verifico i multipli di 5
-    if (i % 5 == 0)
+    else if (i % 5 === 0 && i % 15 !== 0) {
         console.log("Buzz")
+    }
     // Verifico i multipli di 15
-    if (i % 15 == 0)
+    else if (i % 15 == 0) {
         console.log("FizzBuzz")
-    else
+    }
+
+    else {
         console.log(i);
+    }
 }
