c192ea5d08bb1744e328789404112a75e1d2f25f
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-03 10:38:26 +0100

    Replaced innerHTML widh append

--- js/main.js
+++ js/main.js
@@ -1,26 +1,44 @@
+"use strict"
+
 const boardElementBox = document.querySelector(".scacchiera");
+let inside
 
 // Imposto il ciclo for per conteggiare da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
-
     // Verifico i multipli di 15
     if (i % 15 === 0) {
-        boardElementBox.innerHTML += `<div class="quadrato fizzbuzz-color">${"FizzBuzz"}</div>`;
+        const inside = document.createElement("div")
+        inside.classList.add("quadrato","fizzbuzz-color")
+        inside.innerHTML = "FizzBuzz"
+        boardElementBox.append(inside)
         console.log("FizzBuzz")
     }
     // Verifico i multipli di 5
     else if (i % 5 === 0) {
-        boardElementBox.innerHTML += `<div class="quadrato buzz-color">${"Buzz"}</div>`;
+        const inside = document.createElement("div")
+        inside.classList.add("quadrato", "buzz-color")
+        inside.innerHTML = "Buzz"
+        boardElementBox.append(inside)
         console.log("Buzz")
     }
     // Verifico i multipli di 3
     else if (i % 3 === 0) {
-        boardElementBox.innerHTML += `<div class="quadrato fizz-color">${"Fizz"}</div>`;
+        const inside = document.createElement("div")
+        inside.classList.add("quadrato", "fizz-color")
+        inside.innerHTML = "Fizz"
+        boardElementBox.append(inside)
         console.log("Fizz")
     }
     else {
-        boardElementBox.innerHTML += `<div class="quadrato base-color">${i}</div>`;
+        const inside = document.createElement("div")
+        inside.classList.add("quadrato", "base-color")
+        inside.innerHTML = i
+        boardElementBox.append(inside)
         console.log(i)
     }
 }
+
+// const nomevariabile = document.createElement("div");
+// nomevariabile.classList.add( "quadrato", "fizzbuzz-color")
+// nomevariabile.innerHTML = "<strong>ciao</strong>""
